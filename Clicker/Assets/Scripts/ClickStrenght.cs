87a88050af5b53a1885c25b7050fafd2c780b222
kousaatsu <121098273+kousaatsu@users.noreply.github.com>
2023-10-19 22:01:16 +0300

    Level

---
+++ Clicker/Assets/Scripts/ClickStrenght.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ClickStrenght : MonoBehaviour
+{
+    private int coins;
+    private int strenght = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CoinsPerSecond", 0, 3); //per 3 seconds starting from second 0
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Coins: " + coins.ToString());
+        Debug.Log("Strenght: " + strenght.ToString());
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (coins >= 3)
+            {
+                strenght++;
+                coins -= 3;
+            }
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            coins += strenght;
+        }
+    }
+
+    private void CoinsPerSecond()
+    {
+        coins++;
+    }
+}
